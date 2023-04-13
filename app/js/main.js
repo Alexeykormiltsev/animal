b5479cb902faf81ac107d3a5acb7fdeb470029d0
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-04-13 18:13:01 +0500

    первый

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+
+    const swiper = new Swiper('.slider', {
+
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {
+            delay: 6000,
+        },
+        speed: 1500,
+        spaceBetween: 40,
+    });
+});
