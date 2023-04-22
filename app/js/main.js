352360c8fa98aa0cfefe647822b72c8f2c133ffe
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-04-22 22:08:49 +0500

    сделал секцию faq

--- app/js/main.js
+++ app/js/main.js
@@ -14,9 +14,19 @@ $(function () {
         navigation: false,
         // If we need pagination
         pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
+            el: '.swiper-pagination',
+            clickable: true,
         },
     });
-  
+
+    $('#text-link,#img').on('click', function () {
+        $('.faq__answer').toggleClass('faq__answer--active');
+        $('#img').toggleClass('img--active');
+    });
+   
 });
+// document.querySelector('#text-link,#img').on('click', function () {
+//     document.querySelector('.faq__answer').classList.toggle('faq__answer--active');
+//     document.querySelector('#img').classList.toggle('img--active');
+// });
+
