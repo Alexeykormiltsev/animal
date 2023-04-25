0ec0ebb73c7391652ac463ced90fe32e44bfe6a8
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-04-25 16:09:00 +0500

    доделал десктоп

--- app/js/main.js
+++ app/js/main.js
@@ -23,7 +23,23 @@ $(function () {
         $('.faq__answer').toggleClass('faq__answer--active');
         $('#img').toggleClass('img--active');
     });
-   
+
+
+    $(".menu").on("click", "a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
 });
 // document.querySelector('#text-link,#img').on('click', function () {
 //     document.querySelector('.faq__answer').classList.toggle('faq__answer--active');
